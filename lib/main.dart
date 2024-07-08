4e6d0ff7d2c42f4bf2b60cc4e95e07967c0fd8b3
laylaamr <laylamuhana981@gmail.com>
2024-07-09 02:19:25 +0300

    Updated project with XYZ feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 
  main() {
@@ -20,134 +21,52 @@ class TestPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    
+  
     return  Scaffold(
-        body: ListView(
-          children: [ Container(
+        body: 
+          Container(
           //padding: EdgeInsets.only(top:1), 
            alignment: Alignment.topCenter,
            child: Column(
            children: [
           Text("Gallery", style: TextStyle(fontSize: 30),),
-         Row(children: [
-            Container(
-  margin: EdgeInsets.all(20),
-            width: 160,
-            height: 200,
-            decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('images/img1.jpg'),
-               
-              ))
-            ),
-            Spacer(),
-                     Container(
-  margin: EdgeInsets.all(20),
-            width: 160,
-            height: 200,
-            decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('images/img2.jpg'),
-               
-              ))
-            ),
-           
-
-          ],),
-                  Row(children: [
-            Container(
-  margin: EdgeInsets.all(20),
-            width: 160,
-            height: 200,
-            decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('images/img3.jpg'),
-               
-              ))
-            ),
-            Spacer(),
-                     Container(
-  margin: EdgeInsets.all(20),
-            width: 160,
-            height: 200,
-            decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('images/img1.jpg'),
-               
-              ))
-            ),
-           
-
-          ],),        Row(children: [
-            Container(
-  margin: EdgeInsets.all(20),
-            width: 160,
-            height: 200,
-            decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('images/img1.jpg'),
-               
-              ))
-            ),
-            Spacer(),
-                     Container(
-  margin: EdgeInsets.all(20),
-            width: 160,
-            height: 200,
-            decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('images/img3.jpg'),
-               
-              ))
-            ),
-           
-
-          ],),
-                  Row(children: [
-            Container(
-  margin: EdgeInsets.all(20),
-            width: 160,
-            height: 200,
-            decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('images/img2.jpg'),
-               
-              ))
-            ),
-            Spacer(),
-                     Container(
-  margin: EdgeInsets.all(20),
-            width: 160,
-            height: 200,
-            decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('images/img3.jpg'),
-               
-              ))
-            ),
-           
-
-          ],)
-        ],),),],)
-            );
-         
-        
+    Container(
       
+      child:Expanded( child: GridView.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 8.0,
+            mainAxisSpacing: 8.0,
+            children: List.generate(9, (index) {
+              // Modulus operator to cycle through the three images
+              int imageIndex = index % 3;
+               late String imageUrl;
+              switch (imageIndex) {
+                case 0:
+                  imageUrl = 'images/img1.jpg';
+                  break;
+                case 1:
+                  imageUrl = 'images/img2.jpg';
+                  break;
+                case 2:
+                  imageUrl = 'images/img3.jpg';
+                  break;
+              }
+              return Container(
+                //color: Colors.blueAccent,
+                 margin: EdgeInsets.all(20),
+            width: 160,
+            height: 200,
+            decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                 image: AssetImage(imageUrl),
+              )));
+            }),
+          )),
+    ) 
+        ]),
+      ),
+    );
   }
 }
